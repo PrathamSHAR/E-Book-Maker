e18c382e312da760cce4a3c85703584852355c7e
PrathamSHAR <prathamofficial0101@gmail.com>
2023-05-04 09:31:43 +0530

    rating completed

--- texteditor_upload/models.py
+++ texteditor_upload/models.py
@@ -8,6 +8,21 @@ class books(models.Model):
     genre=models.CharField(max_length=255)
     cover_page=models.FileField(upload_to='coverpages')
     file=models.FileField(upload_to='pdf')
+    rating=models.IntegerField(default=0)
+    hitrate=models.IntegerField(default=0)
+    onestar=models.IntegerField(default=0)
+    twostar=models.IntegerField(default=0)
+    threestar=models.IntegerField(default=0)
+    fourstar=models.IntegerField(default=0)
+    fivestar=models.IntegerField(default=0)
+    avgrating=models.FloatField(default=0.0)
+    oneper=models.IntegerField(default=0)
+    twoper=models.IntegerField(default=0)
+    threeper=models.IntegerField(default=0)
+    fourper=models.IntegerField(default=0)
+    fiveper=models.IntegerField(default=0)
+
+
 
     def __str__(self):
         return f"{self.authorname} {self.email} {self.title}"
@@ -22,6 +37,12 @@ class books(models.Model):
 
 class readrate(models.Model):
     title=models.CharField(max_length=254)
-    rate=models.IntegerField()
+    rating=models.IntegerField()
     hitrate=models.IntegerField()
+    onestar=models.IntegerField()
+    twostar=models.IntegerField()
+    threestar=models.IntegerField()
+    fourstar=models.IntegerField()
+    fivestar=models.IntegerField()
+    genre=models.CharField(max_length=254)
     date_time=models.DateTimeField(default=timezone.now)
--- texteditor_upload/views.py
+++ texteditor_upload/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse,HttpResponseRedirect
 from django.template import loader
-from .models import books
+from .models import books,readrate
 from django.urls import reverse
 from django.http import FileResponse
 
@@ -25,6 +25,8 @@ def addrecord(request):
     
     record = books(authorname=a, email=b,title=c,genre=d,cover_page=request.FILES['cover'],file=request.FILES['pdf'])
     record.save()
+    
+
     return HttpResponseRedirect(reverse('submission'))
 
 def show(request):
@@ -74,5 +76,71 @@ def submission(request):
 ## Manupulation By Pratham
 def rate(request):
      rating=request.GET.get('r')
-     print("here is rating",rating)
+     gettitle=request.GET.get('title')
+     record1=books.objects.get(title=gettitle)
+     totalhits=record1.hitrate
+     record1.hitrate=totalhits+1
+
+     # calculating % of rating
+     one=record1.onestar
+     two=record1.twostar
+     three=record1.threestar
+     four=record1.fourstar
+     five=record1.fivestar
+     match int(rating):
+          case 1:
+               record1.onestar=one+1
+               record1.oneper=((one+1)*100)/(totalhits+1)
+               record1.fourper=((four)*100)/(totalhits+1)
+               record1.twoper=((two)*100)/(totalhits+1)
+               record1.threeper=((three)*100)/(totalhits+1)
+               record1.fiveper=((five)*100)/(totalhits+1)
+               record1.avgrating=((one+1)*1+two*2+three*3+four*4+five*5)/((totalhits+1))
+               
+
+          case 2:
+               record1.twostar=two+1
+               record1.oneper=((one)*100)/(totalhits+1)
+               record1.fourper=((four)*100)/(totalhits+1)
+               record1.twoper=((two+1)*100)/(totalhits+1)
+               record1.threeper=((three)*100)/(totalhits+1)
+               record1.fiveper=((five)*100)/(totalhits+1)
+               record1.avgrating=(one*1+(two+1)*2+three*3+four*4+five*5)/((totalhits+1))
+
+          case 3:
+               record1.threestar=three+1
+               record1.oneper=((one)*100)/(totalhits+1)
+               record1.fourper=((four)*100)/(totalhits+1)
+               record1.twoper=((two)*100)/(totalhits+1)
+               record1.threeper=((three+1)*100)/(totalhits+1)
+               record1.fiveper=((five)*100)/(totalhits+1)
+               record1.avgrating=(one*1+two*2+(three+1)*3+four*4+five*5)/((totalhits+1))
+
+          case 4:
+               record1.fourstar=four+1
+               record1.oneper=((one)*100)/(totalhits+1)
+               record1.fourper=((four+1)*100)/(totalhits+1)
+               record1.twoper=((two)*100)/(totalhits+1)
+               record1.threeper=((three)*100)/(totalhits+1)
+               record1.fiveper=((five)*100)/(totalhits+1)
+               record1.avgrating=(one*1+two*2+three*3+(four+1)*4+five*5)/((totalhits+1))
+
+          case 5:
+               record1.fivestar=five+1
+               record1.oneper=((one)*100)/(totalhits+1)
+               record1.fourper=((four)*100)/(totalhits+1)
+               record1.twoper=((two)*100)/(totalhits+1)
+               record1.threeper=((three)*100)/(totalhits+1)
+               record1.fiveper=((five+1)*100)/(totalhits+1)
+               record1.avgrating=(one*1+two*2+three*3+four*4+(five+1)*5)/((totalhits+1))
+          
+               
+     record1.save()
+
+
+
+
+     print("here is rating",rating,gettitle)
+     return redirect('/bookscollection/')
+
      
